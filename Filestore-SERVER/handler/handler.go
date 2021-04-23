e6b382706451e35d58220fddb6e6e98f12842aca
Marcell <1162065497@qq.com>
2021-04-23 15:53:51 +0800

    获取文件元信息接口

--- Filestore-SERVER/handler/handler.go
+++ Filestore-SERVER/handler/handler.go
@@ -1,11 +1,15 @@
 package handler
 
 import (
+	"Filestore-SERVER/meta"
+	"Filestore-SERVER/util"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 //处理文件上传
@@ -27,17 +31,27 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		defer file.Close()
 
-		newFile,err:=os.Create("/tmp/"+head.Filename)
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "/tmp/"+head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
+
+		newFile,err:=os.Create(fileMeta.Location)
 		if err != nil {
 			fmt.Printf("Failed to create file,err:%s\n",err.Error())
 		}
 		defer newFile.Close()
 
-		_,err=io.Copy(newFile, file)
+		fileMeta.FileSize,err=io.Copy(newFile, file)
 		if err != nil {
 			fmt.Printf("Failed to save data into file,err:%s\n",err.Error())
 			return
 		}
+
+		newFile.Seek(0,0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
 
 		http.Redirect(w,r,"/file/upload/suc",http.StatusFound)
 	}
@@ -47,5 +61,21 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 func UploadSucHandler(w http.ResponseWriter, r*http.Request){
 	io.WriteString(w,"Upload finished")
 }
+
+//获取文件元信息接口
+func GetFileMetaHandler(w http.ResponseWriter, r *http.Request){
+
+	r.ParseForm()
+	filehash:=r.Form["filehash"][0]
+	fMeta:=meta.GetFileMeta(filehash)
+	data,err:=json.Marshal(fMeta)
+	if err!=nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+//下载文件接口
 
 
---
+++ Filestore-SERVER/meta/filemeta.go
@@ -0,0 +1,27 @@
+package meta
+
+//文件元信息结构
+type FileMeta struct {
+	//文件的唯一标志
+	FileSha1 string
+	FileName string
+	FileSize int64
+	Location string
+	UploadAt string
+}
+
+var fileMetas map[string]FileMeta
+
+func init() {
+	fileMetas=make(map[string]FileMeta)
+}
+
+//新增/更新文件元信息
+func UpdateFileMeta(fMeta FileMeta) {
+	fileMetas[fMeta.FileSha1] = fMeta
+}
+
+//通过fileSha1获取文件元信息
+func GetFileMeta(fileSha1 string) FileMeta{
+	return fileMetas[fileSha1]
+}
